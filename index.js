5c50d13d2c05d4f045a534609d2bcbea4d5bd0fc
Leonardo Nascimento <141060396+leodatadev@users.noreply.github.com>
2024-02-28 12:55:14 +0000

    Banco de dados implementado no endpoint Create

--- index.js
+++ index.js
@@ -54,18 +54,16 @@ async function main() {
   app.use(express.json())
 
   // Create -> [POST] /item
-  app.post('/item', function (req, res) {
+  app.post('/item', async function (req, res) {
     // Extraímos o corpo da requisição
-    const body = req.body
+    const item = req.body
 
-    // Pegamos o nome (string) que foi enviado dentro do corpo
-    const item = body.nome
 
-    // Colocamos o nome dentro da lista de itens
-    lista.push(item)
+    // Colocamos o nome dentro da collection de itens
+    await collection.insertOne(item)
 
     // Enviamos uma resposta de sucesso
-    res.send('Item adicionado com sucesso')
+    res.send(item)
   })
 
   app.listen(3000)
